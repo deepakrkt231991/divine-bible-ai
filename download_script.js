ec67391e7eee5cd69afc6b8f0274308c1ccbb72c
Deepak Kadam <deepakkadamrkt@gmail.com>
2026-02-28 18:57:40 +0000

    Updated Bible JSON files in public folder

---
+++ download_script.js
@@ -0,0 +1,7 @@
+const files = [
+    {
+      // Updated Hindi IRV Link (Case-sensitive check)
+      url: 'https://bolls.life/static/translations/HINIRV.json', 
+      name: 'hin_irv.json'
+    }
+  ];
