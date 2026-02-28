5d7a67dce378071bf0f20c7f9e1cc08acbb10e99
Deepak Kadam <deepakkadamrkt@gmail.com>
2026-02-28 19:22:59 +0000

    Initial commit for Divine Bible AI

--- download_script.js
+++ download_script.js
@@ -2,6 +2,6 @@ const files = [
     {
       // Updated Hindi IRV Link (Case-sensitive check)
       url: 'https://bolls.life/static/translations/HINIRV.json', 
-      name: 'hin_irv.json'
+      name: 'HIN_IRV.json'
     }
   ];
